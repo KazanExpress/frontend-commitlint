f838d935fd31d310f410b9d27687dfb318b8fb74
Raiondesu <a.raiondesu@gmail.com>
2019-05-17 12:09:21 +0300

    Remove [finalMsg, pattern] index - pattern simplifcation

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const { readFileSync, writeFileSync } = require('fs');
+const { readFileSync } = require('fs');
 
 let gitMessage;
 
@@ -10,27 +10,4 @@ if ('HUSKY_GIT_PARAMS' in process.env) {
   gitMessage = './.git/COMMIT_EDITMSG';
 }
 
-const msg = readFileSync(gitMessage).toString();
-
-const pattern = /^(\w+)(\s+\#\d+)?(\!*)\s*( [\w.].*?)?: (.*)/i;
-
-let finalMsg;
-
-// If the message was not lightly-formatted - forward it to commitlint
-if (!pattern.test(msg)) {
-  finalMsg = msg;
-} else {
-  const matched = msg.match(pattern) || [];
-
-  const type = matched[1] ? matched[1].trim() : '';
-  const issue = matched[2] ? ' ' + matched[2].trim() + ' ' : '';
-  const breaking = matched[3] ? matched[3].trim() : '';
-  const scope = matched[4] ? ` [${matched[4].trim()}]` : '';
-  const subject = matched[5] || '';
-
-  finalMsg = (type ? type[0].toUpperCase() + type.substr(1) : '') + issue + scope + `${breaking}: ${subject}`;
-
-  writeFileSync(gitMessage, finalMsg);
-}
-
-console.log(finalMsg);
+console.log(readFileSync(gitMessage).toString());
